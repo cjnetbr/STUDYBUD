c1148e1cca26881945ead76c4114c1eed99e9d59
Claudio Junior <cjnetbr@hotmail.com>
2024-06-17 15:20:20 +0000

    User Login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -22,16 +23,21 @@ rooms = [
 '''
 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) | 
-        Q(name__icontains=q) | 
-        Q(description__icontains=q)
-    )
+        Q(topic__name__icontains=q) | Q(name__icontains=q)
+        | Q(description__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
@@ -63,6 +69,7 @@ def updateRoom(request, pk):
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
