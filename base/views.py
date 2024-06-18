bd957fd9367ace5579f7286e38841dbf6e906d10
Claudio Junior <cjnetbr@hotmail.com>
2024-06-18 13:44:23 +0000

    User registration

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -25,16 +26,32 @@ rooms = [
     },
 ]
 '''
+
 def logoutUser(request):
     logout(request)
     return redirect('home')
-    
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registration.")
+    return render(request, 'base/login_register.html', {'form':form})
+
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
-        
+
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -47,18 +64,14 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or Password does not exist')
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
-
-    
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) | 
-        Q(name__icontains=q) | 
-        Q(description__icontains=q)
-    )
+        Q(topic__name__icontains=q) | Q(name__icontains=q)
+        | Q(description__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
