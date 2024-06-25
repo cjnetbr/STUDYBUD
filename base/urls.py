51345ffa2c9e062918df680ddbd7fc220add6555
Claudio Junior <cjnetbr@hotmail.com>
2024-06-25 18:17:34 +0000

    Edit User Account Page

--- base/urls.py
+++ base/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+    path('update-user/', views.updateUser, name='update-user'),
+    
 ]
